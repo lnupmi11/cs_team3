2b2a0c302989996f17e8166fdaa22cf56f44673f
Andriana008 <andriana080999@gmail.com>
2018-03-06 16:32:14 +0200

    Add header for the program

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -8,8 +8,22 @@ namespace ChatServer
 {
     class Program
     {
+        public static void ShowHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.WriteLine(" ______     __  __     ______     ______");
+            Console.WriteLine("/\\  ___\\   /\\ \\_\\ \\   /\\  __ \\   /\\__  _\\");
+            Console.WriteLine("\\ \\ \\____  \\ \\  __ \\  \\ \\  __ \\  \\/_/\\ \\/");
+            Console.WriteLine(" \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_\\ \\_\\    \\ \\_\\");
+            Console.WriteLine("  \\/_____/   \\/_/\\/_/   \\/_/\\/_/     \\/_/");
+
+            Console.ResetColor();
+        }
         static void Main(string[] args)
         {
+            ShowHeader();
+            Console.ReadKey();
         }
     }
 }
