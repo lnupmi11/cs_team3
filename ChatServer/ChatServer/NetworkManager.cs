f5699f35ec7e50921c330c6b19eb5d791f4c02b1
Andriana008 <andriana080999@gmail.com>
2018-03-15 23:30:19 +0200

    add function of reading a message from client

---
+++ ChatServer/ChatServer/NetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    public static class NetworkManager
+    {
+        //sending message needs a protocol
+
+        public static string ReadMessage(TcpClient client, int messageLength)
+        {
+            NetworkStream stream = client.GetStream();
+
+            byte[] receiveBuffer = new byte[messageLength];
+
+            int receivedBytes = stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+
+            string message = Encoding.ASCII.GetString(receiveBuffer, 0, receivedBytes);
+
+            return message;
+        }
+    }
+}
