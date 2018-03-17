bc64cb2129b0895abf7532454763a6808a0fd689
mariia.hapatyn <mariia.hapatyn@gmail.com>
2018-03-17 20:57:27 +0200

    created class User

---
+++ ChatServer/ChatServer/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ChatServer {
+	class User {
+		public User ( string username, TcpClient client, string sessionkey ) {
+			this.Username = username;
+			this.Client = client;
+			this.SessionKey = sessionkey;
+		}
+
+		public string Username {
+			get;
+			set;
+		}
+
+		public TcpClient Client {
+			get;
+			set;
+		}
+
+		public string SessionKey {
+			get;
+			set;
+		}
+	}
+}
