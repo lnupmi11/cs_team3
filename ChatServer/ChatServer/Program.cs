36b73c94b2bccb0891175dbb4d85be1b6b706d25
nataliapachva <natalia.pachva@gmail.com>
2018-04-16 22:41:05 +0300

    Implementation Server logic

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -9,38 +9,10 @@ namespace ChatServer
 {
     class Program
     {
-        private static bool serverRunning = false;
-
-        private static TcpListener listener;
-
         static void Main(string[] args)
         {
             Server server = new Server();
             server.Start();
-            server.Stop();
-
-            ShowHeader();
-
-            Console.WriteLine();
-            Console.WriteLine("Press [S] to start server. Press [Q] to stop server. Press [E] to exit");
-
-            ConsoleKeyInfo cki = Console.ReadKey(true);
-            while (cki.Key != ConsoleKey.E)
-            {
-                if (cki.Key == ConsoleKey.S)
-                {
-                    StartServer();
-                }
-                else if (cki.Key == ConsoleKey.Q)
-                {
-                    StopServer();
-                }
-                else
-                {
-                    Console.WriteLine("You have pressed wrong key");
-                }
-                cki = Console.ReadKey(true);
-            } 
         }
 
         public static void ShowHeader()
@@ -54,66 +26,6 @@ namespace ChatServer
             Console.WriteLine("  \\/_____/   \\/_/\\/_/   \\/_/\\/_/     \\/_/");
 
             Console.ResetColor();
-
-        }
-
-        public static void StartServer()
-        {
-            if (serverRunning == true)
-            {
-                Console.WriteLine("Server is already running");
-            }
-            else
-            {
-
-                //listener = new TcpListener(IPAddress, port);
-
-                serverRunning = true;
-                
-                Console.WriteLine("The sever has been started");
-
-            
-            }
-        }
-
-        public static void StopServer()
-        {
-            if (serverRunning == true)
-            {
-                serverRunning = false;
-
-                //listener.Stop();
-
-                Console.WriteLine("Server has been stopped");
-            }
-            else
-            {
-                Console.WriteLine("Server is stopped already");
-            }
-        }
-
-        public static void StartListening()
-        {
-            listener.Start();
-
-            while (serverRunning == true)
-            {            
-                TcpClient client = listener.AcceptTcpClient(); 
-                             
-            }
-        }
-
-        public static void HandleNewSession(object data)
-        {
-            TcpClient client = (TcpClient)data;
-
-            NetworkStream stream = client.GetStream();
-
-            if (stream.CanWrite && stream.CanRead)
-            {
-               //get a stream object for read and write
-               //connect to some class in Client
-            }
         }
     }
 }
--- ChatServer/ChatServer/Server.cs
+++ ChatServer/ChatServer/Server.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ChatServer
@@ -11,6 +12,7 @@ namespace ChatServer
     public class Server
     {
         private TcpListener tcpListener;
+        private List<Client> clients = new List<Client>();
 
         public void Start()
         {
@@ -20,23 +22,43 @@ namespace ChatServer
             tcpListener.Start();
             Console.WriteLine("Server started.");
 
-            TcpClient tcpClient = tcpListener.AcceptTcpClient(); 
-            Console.WriteLine("New client connected.");
-            ReadMessage(tcpClient);
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                Client client = new Client(tcpClient, this);
+                clients.Add(client);
+                Thread clientThread = new Thread(new ThreadStart(client.Listen));
+                clientThread.Start();
+            }
         }
 
-        public void ReadMessage(TcpClient tcpClient)
+        public void SendMessageToAllClients(string message, string id)
         {
-            NetworkStream stream = tcpClient.GetStream();
-            byte[] data = new byte[256];
-            int bytes = stream.Read(data, 0, data.Length); 
-            string message = Encoding.UTF8.GetString(data, 0, bytes);
-            Console.WriteLine(message);
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if (clients[i].Id != id)
+                {
+                    clients[i].Stream.Write(data, 0, data.Length);
+                }
+            }
         }
-        
+
+        public void DeleteConnection(string id)
+        {
+            Client client = clients.FirstOrDefault(c => c.Id == id);
+            if (client != null)
+            {
+                clients.Remove(client);
+            }
+        }
+
         public void Stop()
         {
-            tcpListener.Stop();
+            if (tcpListener != null)
+            {
+                tcpListener.Stop();
+            }
             Console.WriteLine("Server stoped.");
         }
     }
