b3b910432f9b98a6cb9e711898edd1b6153f3582
nataliapachva <natalia.pachva@gmail.com>
2018-05-10 22:42:31 +0300

    Added run exe

---
+++ RunExe/RunExe/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace RunExe
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter n");
+            int size = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < size; i++)
+            {
+                Process.Start("ChatClient.exe");
+            }
+        }
+    }
+}
