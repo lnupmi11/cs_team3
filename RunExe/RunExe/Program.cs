d42a7df15ea3deed3727c16e75bee7a411201a87
MariiaHapatyn <mariia.hapatyn@gmail.com>
2018-05-16 19:42:53 +0300

    Added restrictions on the number of users

--- RunExe/RunExe/Program.cs
+++ RunExe/RunExe/Program.cs
@@ -9,9 +9,16 @@ namespace RunExe
         {
             Console.WriteLine("Enter number of customers:");
             int size = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < size; i++)
+            if( size > 100 ) 
             {
-                Process.Start("ChatClient.exe");
+                Console.WriteLine( "The maximum number of customers is 100. Please, enter the number from 1 to 100." );
+            }
+            else
+            {
+                for( int i = 0; i < size; i++ ) 
+                {
+                    Process.Start( "ChatClient.exe" );
+                }
             }
         }
     }
